01cc419788498f3122bd2cb6abc2ceeb886edd03
Josue Coronado <a2019210771@estudiantes.upsa.edu.bo>
2021-10-13 17:59:17 -0400

    DbContext agregado al starup

--- Startup.cs
+++ Startup.cs
@@ -2,11 +2,13 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
+using Parcial2Naipe.Data;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -26,6 +28,8 @@ namespace Parcial2Naipe
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddDbContext<AppDbContext>(option =>
+            option.UseSqlServer(Configuration.GetConnectionString("DbContext")));
 
             services.AddControllers();
             services.AddSwaggerGen(c =>
