79749282f8932eb564d41a8524cfc280bec98183
Josue Coronado <a2019210771@estudiantes.upsa.edu.bo>
2021-10-13 17:49:32 -0400

    Agregar archivos de proyecto.

---
+++ Models/Naipe.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Parcial2Naipe.Models
+{
+    public class Naipe
+    {
+        [Key]
+        public string NaipeID{ get; set; }
+        [Required(AllowEmptyStrings =false, ErrorMessage ="Requerido")]
+        
+        public string Nombre { get; set; }
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Requerido")]
+
+        public string Enlace { get; set; }
+    }
+}
